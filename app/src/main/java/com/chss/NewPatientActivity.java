409866eac05cc39029281f7049c62f0983a69006
Tsietsi <tsietsi.ramkatsa@wits.ac.za>
2016-12-12 19:16:00 +0200

    Initial commit

---
+++ app/src/main/java/com/chss/NewPatientActivity.java
@@ -0,0 +1,13 @@
+package com.chss;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NewPatientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_patient);
+    }
+}
---
+++ app/src/main/java/com/chss/PatientSearchActivity.java
@@ -0,0 +1,13 @@
+package com.chss;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PatientSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_search);
+    }
+}
